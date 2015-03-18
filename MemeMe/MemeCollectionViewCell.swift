c024baa0a5a00c6cfac8b5bc079ae9c9de518584
Ashish <ashish@a.patel7734@gmail.com>
2015-03-18 15:37:43 -0700

    add collectionview + configure with data

---
+++ MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Ashish Patel on 3/18/15.
+//  Copyright (c) 2015 Average Techie. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+}
--- MemeMe/SentMemesCollectionViewController.swift
+++ MemeMe/SentMemesCollectionViewController.swift
@@ -8,28 +8,47 @@
 
 import UIKit
 
-class SentMemesCollectionViewController: UIViewController {
-
+class SentMemesCollectionViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    var memes: [Meme]!
+    @IBOutlet weak var memeCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        memes = appDelegate.memes
+        
+        memeCollectionView.delegate = self
+        memeCollectionView.dataSource = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    //MARK: -UICollectionView methods
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier("memecollectioncell", forIndexPath: indexPath) as MemeCollectionViewCell
+        
+        cell.memeImageView.image = self.memes[indexPath.row].memeImage
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
